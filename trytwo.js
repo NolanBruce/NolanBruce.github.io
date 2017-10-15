a4fdcc7fc053dcce3702f7006ed9c2db3a85dbc9
nmbruce <nmbruce@asu.edu>
2017-10-15 16:35:21 -0400

    removed try2

--- trytwo.js
+++
@@ -1,5 +0,0 @@
-    try{
-    var canvas = document.createElement( 'canvas' ); 
-    return !! window.WebGLRenderingContext && ( 
-         canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) );
-   }catch( e ) { return false; }
