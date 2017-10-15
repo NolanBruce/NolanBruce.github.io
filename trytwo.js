2f48766988fcb9355e2ee838ef7248562d6f98f7
nmbruce <nmbruce@asu.edu>
2017-10-15 16:27:24 -0400

    testing2

--- trytwo.js
+++ trytwo.js
@@ -1,7 +1,5 @@
-function webgl_support() { 
-   try{
+    try{
     var canvas = document.createElement( 'canvas' ); 
     return !! window.WebGLRenderingContext && ( 
          canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) );
-   }catch( e ) { return false; } 
- };
+   }catch( e ) { return false; }
